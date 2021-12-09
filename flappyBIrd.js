d35718e57334d66a43056720cee4084c7f4abab5
Pratik Amatya <pratikamatya78@gmail.com>
2021-12-09 19:28:08 +0545

    main: merged motion n flap n gravity

--- flappyBIrd.js
+++ flappyBIrd.js
@@ -4,41 +4,58 @@ let d = 0;
 let f = 197;  // force of 1 flap in newton
 let m = .5;  // mass of bird in kg
 
-// let flapstart = 0
-// let flapend = 0
+let flapstart = 0
+let flapend = 0
 
-// let flaptime = flapend - flapstart
+let flaptime = flapend - flapstart
 // let flapping = false
 
 console.log(bird);
 let gamestop = false
-let birdstyle = bird.currentStyle || window.getComputedStyle(bird);
+let birdstyle = bird.currentStyle || window.getComputedStyle(bird) 
   
 // let startrendertime = 0
 let previousrendertime
 let timesincelastrender = 0
+
+const pipes = document.createElement('div')
+pipes.classList.add('hurdle')
+
+pipes.innerHTML = '<div id="toppipe"></div><div id="gap"></div><div id="bottompipe"></div>'
+
+pipes.style.marginLeft = Math.floor(Math.random()*screen.width) + 'px'
+
+document.getElementById('game').appendChild(pipes) 
+
+const hurdle = document.getElementsByClassName('hurdle')[0] 
+  
 
 function main(timestamp){
     if( previousrendertime === undefined){
         previousrendertime = timestamp
     }
     timesincelastrender = (timestamp - previousrendertime)/1000
- 
-    // document.getElementById('game').appendChild(pipes)
- 
+   
     let reqframe = window.requestAnimationFrame(main) 
 
     if(gamestop){
         window.cancelAnimationFrame(reqframe)
         return
     }
+    else if(parseInt(pipes.style.marginLeft.replace('px','')) <= -50){
+         
+        console.log('looping'); 
+        hurdle.style.marginLeft = screen.width+'px'
+    }
 
-    // if(flaptime > 0){
-    //     v = gravity() + flap()  
-    // }
-    // else{
+    hurdle.style.marginLeft = parseInt(pipes.style.marginLeft.replace('px','')) - 2 +'px'
+    
+    if(flaptime > 0){
+        v = gravity() + flap()  
+    }
+    else{
         v = gravity()
-    // } 
+    } 
     checkgameover()
 
     previousrendertime = timestamp
@@ -78,18 +95,18 @@ function checkgameover(){
 window.requestAnimationFrame(main)
 
 window.addEventListener('mousedown',()=>{
-    // flapstart = new Date().getMilliseconds
+    flapstart = new Date().getMilliseconds
     // flapping = true
 })
 
 window.addEventListener('dblclick',()=>{
     f = 380
-    // flapstart = new Date().getMilliseconds
+    flapstart = new Date().getMilliseconds
     // flapping = true
 })
 
 window.addEventListener('mouseup',()=>{
-    // flapend = new Date().getMilliseconds
+    flapend = new Date().getMilliseconds
     f = 197
     // flapping = false
 })
