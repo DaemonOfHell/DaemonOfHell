e85d81c8f64c4b40893c35afacd48199bc3620b6
Pratik Amatya <pratikamatya78@gmail.com>
2021-12-09 17:01:37 +0545

    gravity: added animation frames for every possible ms

--- flappyBIrd.js
+++ flappyBIrd.js
@@ -1,7 +1,48 @@
 let v = 0;
 let g = 10;
 let d = 0;
-
-
-
 console.log(bird);
+let gamestop = false
+let birdstyle = bird.currentStyle || window.getComputedStyle(bird);
+  
+// let startrendertime = 0
+let previousrendertime
+let timesincelastrender = 0
+
+function main(timestamp){
+    if( previousrendertime === undefined){
+        previousrendertime = timestamp
+    }
+    timesincelastrender = timestamp - previousrendertime
+
+    window.requestAnimationFrame(main)
+    if(gamestop){
+        return
+    }
+
+    gravity()
+    checkgameover()
+
+    previousrendertime = timestamp
+}
+
+function gravity(){
+    d = v*timesincelastrender/1000
+    // bird.style.marginTop = birdstyle.marginTop + 9 + 'px'
+    v = v + g*timesincelastrender
+    bird.style.marginTop = Math.floor(d) + 'px'
+    console.log(timesincelastrender+' : ' +Math.floor(d));
+}
+
+function stopgame(){
+    gamestop = true
+}
+
+function checkgameover(){
+    if(parseInt(birdstyle.marginTop.replace('px','')) > screen.height){
+        gamestop = true
+        document.getElementById('game').innerHTML = 'gameover'
+    } 
+}
+
+window.requestAnimationFrame(main)
