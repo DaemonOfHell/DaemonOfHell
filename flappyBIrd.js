14cdb9128825babd28309f43b17694e8f1506d4b
Pratik Amatya <pratikamatya78@gmail.com>
2021-12-09 19:11:45 +0545

    flap: added flap feature

--- flappyBIrd.js
+++ flappyBIrd.js
@@ -1,6 +1,15 @@
 let v = 0;
 let g = 30;
 let d = 0;
+let f = 197;  // force of 1 flap in newton
+let m = .5;  // mass of bird in kg
+
+// let flapstart = 0
+// let flapend = 0
+
+// let flaptime = flapend - flapstart
+// let flapping = false
+
 console.log(bird);
 let gamestop = false
 let birdstyle = bird.currentStyle || window.getComputedStyle(bird);
@@ -13,26 +22,41 @@ function main(timestamp){
     if( previousrendertime === undefined){
         previousrendertime = timestamp
     }
-    timesincelastrender = timestamp - previousrendertime
+    timesincelastrender = (timestamp - previousrendertime)/1000
 
-    window.requestAnimationFrame(main)
+    let reqframe = window.requestAnimationFrame(main)
 
     if(gamestop){
+        window.cancelAnimationFrame(reqframe)
         return
     }
 
-    gravity()
+    // if(flaptime > 0){
+    //     v = gravity() + flap()  
+    // }
+    // else{
+        v = gravity()
+    // } 
     checkgameover()
 
     previousrendertime = timestamp
 }
 
 function gravity(){
-    d = v 
+    d = Math.floor(v) 
     // bird.style.marginTop = birdstyle.marginTop + 9 + 'px'
-    v = v + g * timesincelastrender/1000
-    bird.style.marginTop = parseInt(birdstyle.marginTop.replace('px','')) + Math.floor(d) + 'px'
-    console.log(timesincelastrender/1000+' : ' +Math.floor(d));
+    v = v + g * timesincelastrender
+    bird.style.marginTop = parseInt(birdstyle.marginTop.replace('px','')) + d + 'px'
+    // console.log(timesincelastrender+' : ' +d);
+return v
+}
+
+function flap(){
+    // console.log('flap: ',(f*timesincelastrender)/m+v);
+    // bird.style.backgroundImage = "url('./res/birdflap.gif')"
+    // bird.style.transform = "rotate(335deg)";
+    v =  -(f*timesincelastrender)/m  
+return v
 }
 
 function stopgame(){
@@ -46,4 +70,22 @@ function checkgameover(){
     } 
 }
 
+
 window.requestAnimationFrame(main)
+
+window.addEventListener('mousedown',()=>{
+    // flapstart = new Date().getMilliseconds
+    // flapping = true
+})
+
+window.addEventListener('dblclick',()=>{
+    f = 380
+    // flapstart = new Date().getMilliseconds
+    // flapping = true
+})
+
+window.addEventListener('mouseup',()=>{
+    // flapend = new Date().getMilliseconds
+    f = 197
+    // flapping = false
+})
