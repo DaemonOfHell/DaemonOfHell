dffac7ac7fab99ae964d0be88edcc37692e8d356
Pratik Amatya <pratikamatya78@gmail.com>
2021-12-11 13:50:51 +0545

    collision: collision func fixed with flap sounds

--- flappyBIrd.js
+++ flappyBIrd.js
@@ -1,11 +1,11 @@
 let v = 0;
-let g = 30;
+let g = 20;
 let d = 0;
-let f = 197;  // force of 1 flap in newton
+let f = 108;  // force of 1 flap in newton
 let m = .5;  // mass of bird in kg
 
 const bgm = new Audio('./res/sounds/bgm.ogg')
-const sfx_flap = new Audio('./res/sounds/flap2.wav')
+// const sfx_flap = new Audio('./res/sounds/flap2.wav')
 const sfx_gasp = new Audio('./res/sounds/gasp-boy.mp3')
 const sfx_fall = new Audio('./res/sounds/cartoonfall3.wav')
 
@@ -25,27 +25,29 @@ let gamestop = false
 let previousrendertime
 let timesincelastrender = 0
 
+
 const pipes = document.createElement('div')
 pipes.classList.add('hurdle') 
-pipes.innerHTML = '<div id="toppipe"></div><div id="gap"></div><div id="bottompipe"></div>' 
+pipes.innerHTML = '<div class="toppipe"></div><div class="gap"></div><div class="bottompipe"></div>'
 pipes.style.marginLeft = Math.floor(Math.random()*screen.width) + 'px' 
-document.getElementById('game').appendChild(pipes) 
+document.getElementById('game').appendChild(pipes)  
 
 const pipes2 = document.createElement('div')
 pipes2.classList.add('hurdle') 
-pipes2.innerHTML = '<div id="toppipe"></div><div id="gap"></div><div id="bottompipe"></div>' 
-pipes2.style.marginLeft = Math.floor(Math.random()*screen.width) + 'px' 
+pipes2.innerHTML = '<div class="toppipe"></div><div class="gap"></div><div class="bottompipe"></div>'
+pipes2.style.marginLeft = (pipes.offsetLeft + screen.width/2)+'px' 
 document.getElementById('game').appendChild(pipes2) 
- 
-const hurdle = document.getElementsByClassName('hurdle') 
-  
+    
+const bird = document.getElementById('bird')
 let birdstyle = bird.currentStyle || window.getComputedStyle(bird) 
 
-const tp = document.getElementById('toppipe')
-const bp = document.getElementById('bottompipe')
+const toppipes = document.getElementsByClassName('toppipe')
+const gap = document.getElementsByClassName('gap') 
+const bottompipes = document.getElementsByClassName('bottompipe') 
 
 function main(timestamp){
-
+    const hurdle = document.getElementsByClassName('hurdle') 
+ 
     if( previousrendertime === undefined){ 
         previousrendertime = timestamp
     }
@@ -62,14 +64,17 @@ function main(timestamp){
             return
         } 
     }
-    else if(parseInt(pipes.style.marginLeft.replace('px','')) <= -50){
+    else if(pipes.getBoundingClientRect().right < 0){
          
         console.log('looping'); 
+        toppipes[0].style.height = Math.floor(Math.random()*screen.height/2) + 'px'
+        bottompipes[0].style.height = screen.height - gap[0].style.height - toppipes[0].getBoundingClientRect().bottom + 'px'
         hurdle[0].style.marginLeft = screen.width+'px'
     }
-    else if(parseInt(pipes2.style.marginLeft.replace('px','')) <= -50){
-         
+    else if(pipes2.getBoundingClientRect().right < 0){
+        toppipes[1].style.height = Math.floor(Math.random()*screen.height/2) + 'px'
         console.log('looping'); 
+        bottompipes[1].style.height = screen.height - gap[1].getBoundingClientRect().bottom + 'px'
         hurdle[1].style.marginLeft = screen.width+'px'
     }
 
@@ -78,6 +83,8 @@ function main(timestamp){
 
     if(flaptime > 0){
         v = gravity() + flap()  
+        flapstart = 0
+        flapend = 0
     }
     else{
         v = gravity()
@@ -88,16 +95,17 @@ function main(timestamp){
 }
 
 function gravity(){ 
-    d = Math.floor(v) 
-    // bird.style.marginTop = birdstyle.marginTop + 9 + 'px'
+    d = Math.floor(v)  
     v = v + g * timesincelastrender
-    bird.style.marginTop = parseInt(birdstyle.marginTop.replace('px','')) + d + 'px'
-    // console.log(timesincelastrender+' : ' +d);
+    console.log(timesincelastrender+': '+d);
+    bird.style.marginTop = parseInt(birdstyle.marginTop.replace('px','')) + d + 'px'  
 return v
 }
 
 function flap(){ 
+    
     v =  -(f*timesincelastrender)/m  
+    new Audio('./res/sounds/flap2.wav').play()
 return v 
 }
 
@@ -107,8 +115,7 @@ function stopgame(){
 
 function checkgameover(){
  
-    if(parseInt(birdstyle.marginTop.replace('px','')) > screen.height){
-        let choice;
+    if(parseInt(birdstyle.marginTop.replace('px','')) > screen.height){ 
         sfx_fall.play()
         gamestop = true
         document.getElementById('game').innerHTML = 'gameover' 
@@ -116,29 +123,39 @@ function checkgameover(){
     collision()
 }
 
-function collision(){
-    // let birdstyle = bird.currentStyle || window.getComputedStyle(bird) 
+function collision(){ 
 
-let birdrect = bird.getBoundingClientRect()
+    let birdrect = bird.getBoundingClientRect()
+        
+    let pipe1top = toppipes[0].getBoundingClientRect();
+    let pipe2top = toppipes[1].getBoundingClientRect();
     
-let pipe_1 = tp.getBoundingClientRect();
-let pipe_2 = bp.getBoundingClientRect();
-
-console.log(birdrect.height, birdrect.width); 
-    if(birdrect.top + birdrect.width/3 < pipe_1.bottom && birdrect.right - birdrect.width/3 > pipe_1.left &&
-        birdrect.left + birdrect.width/3 < pipe_1.right ){  
-        gamestop = true
-        // bird.style.height = '5vmin'
-        sfx_gasp.play()
+    let pipe1bottom = bottompipes[0].getBoundingClientRect();
+    let pipe2bottom = bottompipes[1].getBoundingClientRect();
+    
+    console.log(birdrect.height, birdrect.width); 
+        if(birdrect.top + birdrect.width/3 < pipe1top.bottom && birdrect.right - birdrect.width/3 > pipe1top.left &&
+            birdrect.left + birdrect.width/3 < pipe1top.right ){  
+            gamestop = true
+            sfx_gasp.play()
+        }
+        else if(birdrect.top + birdrect.width/3 < pipe2top.bottom && birdrect.right - birdrect.width/3 > pipe2top.left &&
+            birdrect.left + birdrect.width/3 < pipe2top.right ){  
+            gamestop = true
+            sfx_gasp.play()
+        } 
+        else if(birdrect.bottom - birdrect.width/3 > pipe1bottom.top && birdrect.right - birdrect.width/3 > pipe1bottom.left &&
+            birdrect.left + birdrect.width/3 < pipe1bottom.right ){  
+            gamestop = true
+            sfx_gasp.play()
+        }
+        else if(birdrect.bottom - birdrect.width/3 > pipe2bottom.top && birdrect.right - birdrect.width/3 > pipe2bottom.left &&
+            birdrect.left + birdrect.width/3 < pipe2bottom.right ){  
+            gamestop = true
+            sfx_gasp.play()
+        }
+      
     }
-    if(birdrect.bottom - birdrect.width/3 > pipe_2.top && birdrect.right - birdrect.width/3 > pipe_2.left &&
-        birdrect.left + birdrect.width/3 < pipe_2.right ){  
-        gamestop = true
-        // bird.style.height = '5vmin'
-        sfx_gasp.play()
-    }
-  
-}
 
 
 window.requestAnimationFrame(main)
@@ -147,18 +164,11 @@ window.addEventListener('mousedown',()=>{
     flapstart = new Date().getMilliseconds
     // flapping = true
      sfx_flap.play()
-})
-
-window.addEventListener('dblclick',()=>{
-    // sfx_flap.play()
-    f = 480
-    flapstart = new Date().getMilliseconds
-    // flapping = true
-})
+}) 
 
 window.addEventListener('mouseup',()=>{
     // sfx_flap.play()
     flapend = new Date().getMilliseconds
-    f = 197
+    // f = 2
     // flapping = false
 })
